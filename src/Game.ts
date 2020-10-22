c8141c4cc0974d8ef4ff8cf6dfa0565810efd4db
Thomas Abbondi <digital.crystalware@gmail.com>
2020-10-22 23:18:27 +0200

    First project step + fix Phaser import

--- src/Game.ts
+++ src/Game.ts
@@ -1,16 +1,39 @@
-import 'phaser';
+import * as Phaser from 'phaser';
+
 import { Preloader } from './scenes/Preloader';
 import { Main } from './scenes/Main';
 
-const config: GameConfig = {
+const config: Phaser.Types.Core.GameConfig = {
+  title: 'Torch',
   type: Phaser.AUTO,
   parent: 'canvas',
+  backgroundColor: '#AFAFAF',
+
   width: 960,
   height: 540,
+
+  resolution: 1,
+  zoom: 1,
+
+  // scale: {
+  //   width: window.innerWidth,
+  //   height: window.innerHeight,
+  // },
+
   scene: [
     Preloader,
     Main
-  ]
+  ],
+
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true,
+      gravity: {
+        y: 64
+      }
+    }
+  }
 };
 
-const game = new Phaser.Game(config);
+export const game = new Phaser.Game(config);
--- src/scenes/Main.ts
+++ src/scenes/Main.ts
@@ -1,23 +1,53 @@
+const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
+  active: true,
+  visible: false,
+  key: 'Game',
+};
+
 export class Main extends Phaser.Scene {
+  private hero!: Phaser.Physics.Arcade.Sprite;
+
   constructor() {
-    super('main');
+    super(sceneConfig);
   }
 
-  create() {
-    const logo = this.add.sprite(
-      this.sys.canvas.width / 2,
-      this.sys.canvas.height / 2,
-      'sheet1',
-      'phaser-logo'
-    );
-
-    const music = this.sound.add('DOG');
-    this.input.on('pointerdown', (pointer: Phaser.Input.Pointer) => {
-      if (!music.isPlaying) {
-        music.play();
-      }
-
-      logo.setPosition(pointer.x, pointer.y);
+  preload() {
+    this.load.spritesheet('hero', '/assets/sprites/hero.png', {
+      frameWidth: 20,
+      frameHeight: 32
     });
+  }
+
+  create(): void {
+    this.hero = this.physics.add.sprite(
+      32,
+      this.sys.game.canvas.height / 2,
+      'hero'
+    );
+    this.hero.setOrigin(0, 0);
+
+    this.physics.add.existing(this.hero);
+  }
+
+  update(): void {
+    const cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys =
+      this.input.keyboard.createCursorKeys();
+    const heroBody = this.hero.body as Phaser.Physics.Arcade.Body;
+
+    if (cursorKeys.up!.isDown) {
+      heroBody.setVelocityY(-500);
+    } else if (cursorKeys.down!.isDown) {
+      heroBody.setVelocityY(500);
+    } else {
+      heroBody.setVelocityY(0);
+    }
+
+    if (cursorKeys.right!.isDown) {
+      heroBody.setVelocityX(500);
+    } else if (cursorKeys.left!.isDown) {
+      heroBody.setVelocityX(-500);
+    } else {
+      heroBody.setVelocityX(0);
+    }
   }
 }
