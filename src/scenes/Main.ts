8b44d375cd9f214e6f930f5818e974a96d44b2ae
Thomas Abbondi <digital.crystalware@gmail.com>
2020-10-25 22:17:11 +0100

    First split of Player logic from Scene

--- src/scenes/Main.ts
+++ src/scenes/Main.ts
@@ -4,8 +4,10 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
   key: 'Game',
 };
 
+import { Player } from '../entities/Player';
+
 export class Main extends Phaser.Scene {
-  private hero!: Phaser.Physics.Arcade.Sprite;
+  private hero!: Player;
   private background!: Phaser.GameObjects.TileSprite;
   private isJumping = false;
   private isFacing!: any;
@@ -14,45 +16,15 @@ export class Main extends Phaser.Scene {
     super(sceneConfig);
 
     this.isFacing = {
-      x: null,
+      x: 'front',
       y: null,
     }
   }
 
-  preload() {
-    this.load.image('big', '/assets/sprites/big.png');
+  preload(): void {
+    this.load.image('big', '/assets/sprites/tilemap.png');
 
-    const spriteSize: Phaser.Types.Loader.FileTypes.ImageFrameConfig = {
-      frameWidth: 32,
-      frameHeight: 48
-    };
-
-    this.load
-      .spritesheet(
-        'hero_idle',
-        '/assets/sprites/hero_idle.png',
-        spriteSize
-      )
-      .spritesheet(
-        'hero_idle_left',
-        '/assets/sprites/hero_idle_left.png',
-        spriteSize
-      )
-      .spritesheet(
-        'hero_idle_right',
-        '/assets/sprites/hero_idle_right.png',
-        spriteSize
-      )
-      .spritesheet(
-        'hero_walk_left',
-        '/assets/sprites/hero_walk_left.png',
-        spriteSize
-      )
-      .spritesheet(
-        'hero_walk_right',
-        '/assets/sprites/hero_walk_right.png',
-        spriteSize
-      );
+    Player.preload(this);
   }
 
   create(): void {
@@ -65,60 +37,14 @@ export class Main extends Phaser.Scene {
     )
       .setOrigin(0, 0);
 
-    this.hero = this.physics.add.sprite(
-      32,
-      this.sys.game.canvas.height,
-      'hero_idle'
-    );
-    this.hero
-      .setOrigin(0, 0)
-      .setCollideWorldBounds(true)
-      .setBounce(0);
-
-    this.physics.world.enable(this.hero);
-
-    this.anims.create({
-      key: 'hero_idle',
-      frames: this.anims.generateFrameNumbers('hero_idle', {}),
-      frameRate: 20,
-      repeat: -1
-    });
-
-    this.anims.create({
-      key: 'hero_idle_left',
-      frames: this.anims.generateFrameNumbers('hero_idle_left', {}),
-      frameRate: 20,
-      repeat: -1
-    });
-
-    this.anims.create({
-      key: 'hero_idle_right',
-      frames: this.anims.generateFrameNumbers('hero_idle_right', {}),
-      frameRate: 20,
-      repeat: -1
-    });
-
-    this.anims.create({
-      key: 'hero_walk_left',
-      frames: this.anims.generateFrameNumbers('hero_walk_left', {}),
-      frameRate: 20,
-      repeat: -1
-    });
-
-    this.anims.create({
-      key: 'hero_walk_right',
-      frames: this.anims.generateFrameNumbers('hero_walk_right', {}),
-      frameRate: 20,
-      repeat: -1
-    });
+    this.hero = new Player(this, 32, this.sys.game.canvas.height);
   }
 
   update(): void {
     const cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys =
       this.input.keyboard.createCursorKeys();
-    const heroBody = this.hero.body as Phaser.Physics.Arcade.Body;
 
-    if (heroBody.onFloor()) {
+    if (this.hero.body.onFloor()) {
       this.isJumping = false;
 
       if (cursorKeys.up!.isDown) {
@@ -132,17 +58,17 @@ export class Main extends Phaser.Scene {
 
       this.hero
         .setVelocityX(128)
-        .play('hero_walk_right', true);
+        .play('hero_walk_right_animation', true);
     } else if (cursorKeys.left!.isDown) {
       this.isFacing.x = 'left';
 
       this.hero
         .setVelocityX(-128)
-        .play('hero_walk_left', true);
+        .play('hero_walk_left_animation', true);
     } else {
       this.hero
         .setVelocityX(0)
-        .play(`hero_idle_${this.isFacing.x}`, true);
+        .play(`hero_idle_${this.isFacing.x}_animation`, true);
     }
   }
 }
