6347b9a4da5a33b19afdb3af3f2bd4d370d8ea8c
Thomas Abbondi <digital.crystalware@gmail.com>
2020-10-26 23:40:24 +0100

    Implement the double jump + commented the previous code

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -5,9 +5,39 @@ import { ControlScene } from '../scenes/ControlScene';
 export class Player extends Phaser.Physics.Arcade.Sprite {
   public body!: Phaser.Physics.Arcade.Body;
 
+  /**
+   * Where the Player is watching
+   * @type {Facing}
+   */
   private facing: Facing;
+
+  /**
+   * The Player is jumping
+   * @type {Boolean}
+   */
   private isJumping = false;
+
+  /**
+   * The Player is falling
+   * @type {Boolean}
+   */
   private isFalling = false;
+
+  /**
+   * The Player has the ability to do the double jump
+   * @type {Boolean}
+   */
+  private hasDoubleJump = true;
+  /**
+   * The Player can now perform the double jump
+   * @type {Boolean}
+   */
+  private canDoubleJump = false;
+  /**
+   * The Player had actually performed the double jump
+   * @type {Boolean}
+   */
+  private hasDoneDoubleJump = false;
 
   constructor(
     public scene: ControlScene,
@@ -133,24 +163,58 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
   }
 
   protected jump(): void {
+    // Test if the user is pressing the button 'A'
     const isJumpPress = this.scene.isKeyPress('a');
-    this.isFalling = this.body.velocity.y > 0;
 
-    if (isJumpPress && this.body.onFloor()) {
-      this.isJumping = true;
-      this.body.velocity.y = -(256 * 1.25);
+    // If the user is pressing jump button
+    if (isJumpPress) {
+      // If the Player is touching the floor
+      if (this.body.onFloor()) {
+        // The Player is actually jumping
+        this.isJumping = true;
+
+        this.setVelocityY(-256 * 1.25);
+      } else if (
+        // First check if Player has unlocked double jump
+        this.hasDoubleJump
+        // Then check if Player had already performed the action
+        && !this.hasDoneDoubleJump
+        // Finally check if Player can actually perform the action at the moment
+        && this.canDoubleJump
+      ) {
+        // Prevent the Player to perform another double jump
+        this.hasDoneDoubleJump = true;
+        // The Player is actually jumping
+        this.isJumping = true;
+
+        this.setVelocityY(-256 * 1.25);
+      }
     } else if (this.body.onFloor()) {
+      // Reset everything if the player is touching the ground
       this.isFalling = false;
       this.isJumping = false;
+
+      this.canDoubleJump = false;
+      this.hasDoneDoubleJump = false;
     }
 
+    // Detect if the Player is falling by checking if his y speed is positive
     if (this.body.velocity.y > 0) {
       this.isFalling = true;
+      // If the Player is falling, he cannot also jump
       this.isJumping = false;
     }
 
-    if (this.body.velocity.y < 0 && !isJumpPress) {
-      this.body.velocity.y = Math.max(this.body.velocity.y, 0);
+    // Detect if the Player is jumping by checking if his y speed is negative
+    if (this.body.velocity.y < 0) {
+      // Detect if the user is not pressing the button to jump
+      if (!isJumpPress) {
+        // The Player is able to perform a double jump
+        this.canDoubleJump = true;
+
+        // The Player slowly stop to jump and perform a linear fall
+        this.setVelocityY(this.body.velocity.y + 10);
+      }
     }
   }
 }
