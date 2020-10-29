26357edba16633e2dc7acbca7a7b1e09d2fb7c91
Thomas Abbondi <digital.crystalware@gmail.com>
2020-10-29 22:59:14 +0100

    Add bounding walls

--- src/scenes/Main.ts
+++ src/scenes/Main.ts
@@ -10,6 +10,7 @@ import { ControlScene } from './ControlScene';
 export class Main extends ControlScene {
   private hero!: Player;
   private background!: Phaser.GameObjects.TileSprite;
+  private blocks!: Phaser.Physics.Arcade.StaticGroup;
 
   constructor() {
     super(sceneConfig);
@@ -31,9 +32,30 @@ export class Main extends ControlScene {
       this.sys.game.canvas.height,
       'big'
     )
-      .setOrigin(0, 0);
+      .setOrigin(0, 0)
+      .setAlpha(0.5);
 
-    this.hero = new Player(this, 32, this.sys.game.canvas.height);
+    const wallLeft = this.physics.add.staticSprite(0, 0, 'big');
+
+    wallLeft
+      .setOrigin(0, 0)
+      .setImmovable(true)
+      .setScale(1, this.sys.game.canvas.height / wallLeft.height)
+      .refreshBody();
+
+    const wallRight = this.physics.add.staticSprite(this.sys.game.canvas.width - wallLeft.width, 0, 'big');
+
+    wallRight
+      .setOrigin(0, 0)
+      .setImmovable(true)
+      .setScale(1, this.sys.game.canvas.height / wallRight.height)
+      .refreshBody();
+
+    const walls = this.physics.add.staticGroup([wallLeft, wallRight]);
+
+    this.hero = new Player(this, 32, this.sys.game.canvas.height - 64);
+
+    this.physics.world.addCollider(this.hero, walls);
   }
 
   update(): void {
