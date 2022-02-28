fe7794f6c53ef6d793b22d4effa12d2f6bf76b44
Thomas Abbondi <digital.crystalware@gmail.com>
2022-02-28 22:19:57 +0100

    Install webpack merge for unify dev and prod build

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,78 @@
+const path = require('path');
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = (env) => {
-  return require(`./webpack.${env}.js`);
+  const config = {
+    entry: {
+      game: './src/Game.ts'
+    },
+    output: {
+      path: path.resolve(__dirname, 'build'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          use: 'ts-loader'
+        },
+        {
+          test: [ /\.vert$/, /\.frag$/ ],
+          use: 'raw-loader'
+        }
+      ]
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all'
+          }
+        }
+      }
+    },
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ],
+      alias: {
+        Config: path.resolve(__dirname, 'src/Config/'),
+        Plugins: path.resolve(__dirname, 'src/Plugins/'),
+        Entities: path.resolve(__dirname, 'src/Entities/'),
+        Bullets: path.resolve(__dirname, 'src/Entities/Bullets'),
+        Weapons: path.resolve(__dirname, 'src/Entities/Weapons'),
+        Hitboxes: path.resolve(__dirname, 'src/Entities/Hitboxes'),
+        Miscellaneous: path.resolve(__dirname, 'src/Miscellaneous/'),
+        Scenes: path.resolve(__dirname, 'src/Scenes/')
+      }
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'typeof SHADER_REQUIRE': JSON.stringify(false),
+        'typeof CANVAS_RENDERER': JSON.stringify(true),
+        'typeof WEBGL_RENDERER': JSON.stringify(true)
+      }),
+      new CopyWebpackPlugin([
+        {
+          from: './assets',
+          to: './assets',
+          force: true
+        },
+        {
+          from: './app.css',
+          to: './app.css',
+          force: true
+        }
+      ]),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'index.html'
+      })
+    ],
+  };
+
+  return merge(config, require(`./webpack.${env}.js`));
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,75 +1,11 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
 module.exports = {
-  entry: {
-    game: './src/Game.ts'
-  },
   mode: 'production',
   output: {
-    path: path.resolve(__dirname, 'build'),
     publicPath: './',
     filename: '[name].min.js'
   },
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: 'ts-loader'
-      },
-      {
-        test: [ /\.vert$/, /\.frag$/ ],
-        use: 'raw-loader'
-      }
-    ]
-  },
   optimization: {
     minimize: true,
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
-        }
-      }
-    }
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
-    alias: {
-      Config: path.resolve(__dirname, 'src/config/'),
-      Entities: path.resolve(__dirname, 'src/entities/'),
-      Bullets: path.resolve(__dirname, 'src/entities/bullets'),
-      Weapons: path.resolve(__dirname, 'src/entities/weapons'),
-      Miscellaneous: path.resolve(__dirname, 'src/miscellaneous/'),
-      Scenes: path.resolve(__dirname, 'src/scenes/')
-    }
   },
   stats: false,
-  plugins: [
-    new webpack.DefinePlugin({
-      'typeof SHADER_REQUIRE': JSON.stringify(false),
-      'typeof CANVAS_RENDERER': JSON.stringify(true),
-      'typeof WEBGL_RENDERER': JSON.stringify(true)
-    }),
-    new CopyWebpackPlugin([
-      {
-        from: './assets',
-        to: './assets',
-        force: true
-      },
-      {
-        from: './app.css',
-        to: './app.css',
-        force: true
-      }
-    ]),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html'
-    })
-  ]
 };
